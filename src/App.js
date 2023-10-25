efc06cd3db9feed6c70373547c6f94efa9f03e06
saiprasad-hydraboiny <prasadhydraboiny9122@gmail.com>
2023-10-25 11:51:57 +0530

    addingfiles

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App text-3xl font-bold underline">
+       Namaste Everyone.!! Welcome to Netflix-gpt
+    </div>
+  );
+}
+
+export default App;
